479f8502e7c27fffaf9406e8f9989cf699985ab0
Ermana39 <ermanrabacioglu@mail.com>
2026-04-11 22:16:42 +0300

    yaz

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ export const metadata: Metadata = {
   title: "Hoca İle Borsa",
   description: "Borsa, halka arz, temettü, fonlar ve finans içerikleri",
   other: {
-    "google-adsense-account": "ca-pub-9613627679911162",
+    "google-adsense-account": "ca-pub-9613627671991162",
   },
 };
 
