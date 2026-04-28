fd20f9eb755c5c97dc76f844a1e6d556581d117b
Ermana39 <ermanrabacioglu@mail.com>
2026-04-28 09:25:07 +0300

    yaz

--- lib/son-guncellemeler.ts
+++ lib/son-guncellemeler.ts
@@ -103,6 +103,6 @@ export const sonGuncellemeler: SonGuncellemeItem[] = [
   {
     title: "Günlük Faiz Oranları",
     href: "/mevduat-kredi-faizleri",
-    updatedAt: "27.04.2026 10:35",
+    updatedAt: "28.04.2026 09:25",
   },
 ];
