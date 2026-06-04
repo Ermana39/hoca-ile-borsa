8bea9089c6c12d0ebdce77799a4127a5226e4b41
Ermana39 <ermanrabacioglu@mail.com>
2026-06-04 10:22:56 +0300

    yaz

--- app/data/news.ts
+++ app/data/news.ts
@@ -9,7 +9,7 @@ export type NewsItem = {
 export const newsItems: NewsItem[] = [
     {
     id: 978,
-    title: "02 Haziran 2026 Salı KAP Bildirimleri Özeti",
+    title: "03 Haziran 2026 Salı KAP Bildirimleri Özeti",
     href: "/haber/haber-978",
     image: "/haber978.png",
     alt: "Haber 978 görseli",
